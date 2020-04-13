1eaeafc9f6a47e179e0d567ff734f52c4d208d9a
Charlie Brooking <charlie@brooking.id.au>
2020-04-13 10:40:04 +1000

    configure safe / high-throughput producer

--- src/main/java/org/example/tutorial2/TwitterProducer.java
+++ src/main/java/org/example/tutorial2/TwitterProducer.java
@@ -10,10 +10,15 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -30,10 +35,21 @@ public class TwitterProducer {
 
         // create a twitter client
         BlockingQueue<String> queue = new LinkedBlockingQueue<>(1000);
-        Client twitterClient = createTwitterClient(queue);
+        Client twitterClient = createTwitterClient(queue, Lists.newArrayList("bitcoin", "usa", "politics", "sport", "soccer"));
         twitterClient.connect();
 
         // create a kafka producer
+        KafkaProducer<String, String> producer = createKafkaProducer();
+
+        // add a shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Stopping application...");
+            logger.info("Stopping Twitter client...");
+            twitterClient.stop();
+            logger.info("Stopping Kafka producer...");
+            producer.close();
+            logger.info("Done!");
+        }));
 
         // loop to send tweets to kafka
         while (!twitterClient.isDone()) {
@@ -46,13 +62,39 @@ public class TwitterProducer {
             }
             if (msg != null) {
                 logger.info(msg);
+                ProducerRecord<String, String> record = new ProducerRecord<>("twitter_tweets", null, msg);
+                producer.send(record, (recordMetadata, e) -> {
+                    if (e != null) {
+                        logger.error("Something bad happened", e);
+                    }
+                });
             }
         }
 
         logger.info("End of application");
     }
 
-    private Client createTwitterClient(BlockingQueue<String> queue) {
+    private KafkaProducer<String, String> createKafkaProducer() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // configure safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // configure high throughput producer
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+
+        return new KafkaProducer<>(properties);
+    }
+
+    private Client createTwitterClient(BlockingQueue<String> queue, List<String> terms) {
         Hosts hosts = new HttpHosts(Constants.STREAM_HOST);
 
         Authentication authentication = new OAuth1(
@@ -63,7 +105,6 @@ public class TwitterProducer {
         );
 
         StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-        List<String> terms = Lists.newArrayList("kafka");
         endpoint.trackTerms(terms);
 
         ClientBuilder builder = new ClientBuilder()
