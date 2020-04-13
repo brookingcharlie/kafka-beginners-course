375a8818ecb17bbc2e619cf2d6ab778326138e36
Charlie Brooking <charlie@brooking.id.au>
2020-04-14 00:19:37 +1000

    elasticsearch consumer using tweet id for idempotent handling

--- code/kafka-consumer-elasticsearch/src/main/java/org/example/tutorial3/ElasticSearchConsumer.java
+++ code/kafka-consumer-elasticsearch/src/main/java/org/example/tutorial3/ElasticSearchConsumer.java
@@ -1,6 +1,12 @@
 package org.example.tutorial3;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -12,23 +18,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
     private static final Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class);
 
     public static void main(String[] args) throws IOException {
-        RestHighLevelClient client = createClient();
-        String jsonString = "{\"foo\":\"bar\"}";
-        IndexRequest indexRequest = new IndexRequest("twitter", "tweets")
-                .source(jsonString, XContentType.JSON);
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info(id);
-        client.close();
+        RestHighLevelClient elasticSearchClient = createElasticSearchClient();
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                String tweetJsonString = record.value();
+                String tweetId = extractIdFromTweet(tweetJsonString);
+                IndexRequest indexRequest = new IndexRequest("twitter")
+                        .id(tweetId)
+                        .source(tweetJsonString, XContentType.JSON);
+                IndexResponse indexResponse = elasticSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                logger.info("Tweet ID {} saved with ElasticSearch ID {}", tweetId, indexResponse.getId());
+            }
+        }
     }
 
-    private static RestHighLevelClient createClient() {
+    private static RestHighLevelClient createElasticSearchClient() {
         RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
         return new RestHighLevelClient(builder);
+    }
+
+    private static KafkaConsumer<String, String> createConsumer(String topic) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "kafka-demo-elasticsearch");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+    private static String extractIdFromTweet(String tweetJson) {
+        return JsonParser.parseString(tweetJson).getAsJsonObject().get("id_str").getAsString();
     }
 }
